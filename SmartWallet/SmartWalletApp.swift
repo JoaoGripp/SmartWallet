f4d4f7fe7ee609f2131c67d1c41525c9fb75dfee
Joao Gripp <corguinha@gmail.com>
2023-04-28 15:57:57 +0100

    Initial Commit

---
+++ SmartWallet/SmartWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartWalletApp.swift
+//  SmartWallet
+//
+//  Created by Joao Gripp on 28/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SmartWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
